e5cc02c97e76cb6dcb5e0bd3fffa1c5429739031
mjkghost <100220131+mjkghost@users.noreply.github.com>
2022-02-23 00:11:34 +0800

    testpython

---
+++ HW1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+month_paid=int(input("month_paid"))
+if month_paid<=1000:
+    print("boss,i m your daddy")
+elif month_paid<=2000:
+    print("boss,wqnmlgbxxxx")
+elif month_paid<=5000:
+    print("dont talk shit to me")
+elif month_paid<=10000:
+    print("something wrong in his mind, i dont care")
+elif month_paid<=20000:
+    print("whatever he said, i dont care")
+elif month_paid<=30000:
+    print("boss u r so fuking right")
+elif month_paid<=50000:
+    print("i enjoy 996")
+elif month_paid<=100000:
+    print("company is my family")
+elif month_paid>100000:
+    print("company is my family")
+else:
+    print("i quited")
+
+
+# In[ ]:
+
+
+bg_age=int(input("bg_age"))
+if bg_age<25:
+    print("too young")
+elif bg_age>25:
+    print("too old")
+else: 
+    print("u r right")
+
+
+# In[ ]:
+
+
+score=int(input("score"))
+if score<40:
+    print ("E")
+elif score<60:
+    print("D")
+elif score<80:
+    print("C")
+elif score<90:
+    print("B")
+else:
+    print("A")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
